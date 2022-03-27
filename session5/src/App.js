eaf889d79056480c69b8fbd5ba3fbc83db259f43
unknown <xuongvan.1797@gmail.com>
2022-03-27 17:28:49 +0700

    create session 5

---
+++ session5/src/App.js
@@ -0,0 +1,20 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box-total">
+        <div className="box-button">
+          <input type="submit" value="Next"/>
+        </div>
+        <div className="box-color">
+          <div className="box-red"></div>
+          <div className="box-yellow"></div>
+          <div className="box-green"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
