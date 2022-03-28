53c95cbc4d62afa57cd6b803379445383fe3137c
unknown <xuongvan.1797@gmail.com>
2022-03-28 23:20:12 +0700

    done session5

--- session5/src/App.js
+++ session5/src/App.js
@@ -1,16 +1,44 @@
 import "./style.css"
+import { useState } from "react";
 
 function App() {
+  const [redColor, setRedColor] = useState("white");
+  const [yellowColor, setYellowColor] = useState("white");
+  const [greenColor, setGreenColor] = useState("white");
+  const [count, setCount] = useState(0);
+
+  const changeColor = () => {
+    
+    switch (count) {
+      case 0:
+        setRedColor("red");
+        setGreenColor("white");
+        setCount(count + 1);
+        break;
+      case 1:
+        setYellowColor("yellow");
+        setRedColor("white");
+        setCount(count + 1);
+        break;
+      default:
+        setGreenColor("green");
+        setYellowColor("white");
+        setCount(0);
+        break;
+    }
+    console.log(count);
+  };
+  
   return (
     <div className="App">
       <div className="box-total">
         <div className="box-button">
-          <input type="submit" value="Next"/>
+          <input type="submit" value="Next" onClick={changeColor}/>
         </div>
         <div className="box-color">
-          <div className="box-red"></div>
-          <div className="box-yellow"></div>
-          <div className="box-green"></div>
+          <div className="box-red" style={{background: redColor}}></div>
+          <div className="box-yellow" style={{background: yellowColor}}></div>
+          <div className="box-green" style={{background: greenColor}}></div>
         </div>
       </div>
     </div>
