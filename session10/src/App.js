32a74235c965e7186567b15a9f6e1d4e75d9f5cd
unknown <xuongvan.1797@gmail.com>
2022-04-11 22:41:37 +0700

    update code ss10

--- session10/src/App.js
+++ session10/src/App.js
@@ -1,15 +1,14 @@
-import { Routers, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Auth from './components/Auth';
 import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <Routers>
+      <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='/auth' element={<Auth />} />
-        <Route path='/' element={<Home />} />
-
-      </Routers>
+      </Routes>
     </div>
   );
 }
--- session10/src/index.js
+++ session10/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrowsetRouter from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowsetRouter>
+  <BrowserRouter>
     <App />
-  </BrowsetRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
